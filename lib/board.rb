e0d6abb3a5378ebb28a5d105429c961539315ed4
maniginam <maniginam@gmail.com>
2021-05-15 13:56:23 -0500

    game_over? passing

--- lib/board.rb
+++ lib/board.rb
@@ -2,15 +2,31 @@
 
 # board
 class Board
-  attr_accessor :board
+  attr_accessor :board, :wins
 
   def initialize
     @board = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    @wins = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
   end
 
-  def is_game_over?
+  def is_win?
+    is_win = false
+    wins.each do |win|
+      # line = []
+      # win.map { |box| line >> @board[box]}
+      # is_win = true if line.uniq.count == 1
+      is_win = true if @board[win[0]] == @board[win[1]] && @board[win[1]] == @board[win[2]]
+    end
+    is_win
+  end
+
+  def board_full?
+    @board.uniq(&:class)
+  end
+
+  def game_over?
     box = 0
-    box += 1 until board[box].is_a?(Integer) || box < board.size
+    box += 1 until is_win? || board_full?
     board[box].is_a?(String)
   end
 end
--- test/board_test.rb
+++ test/board_test.rb
@@ -5,16 +5,40 @@ require_relative '../lib/board'
 
 # Test program minimax
 class BoardTest < Minitest::Test
-  def test_is_game_over_empty_board
+  def test_game_over_empty_board
     board = Board.new
-    board.board = %i[0 1 2 3 4 5 6 7 8]
-    assert_equal false, board.is_game_over?
+    board.board = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    assert_equal false, board.game_over?
   end
 
-  def test_is_game_over_full_board
+  def test_game_over_cats_game
     board = Board.new
     board.board = %w[X O X X O O O X X]
-    assert_equal true, board.is_game_over?
+    assert_equal true, board.game_over?
   end
+
+  def test_game_over_o_wins
+    board = Board.new
+    board.board = ['X', 'O', 'O', 3, 'O', 'X', 'O', 'X', 'X']
+    assert_equal true, board.game_over?
+  end
+
+  def test_is_win_not
+    board = Board.new
+    board.board = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    assert_equal false, board.game_over?
+  end
+
+  def test_is_win_x_wins
+    board = Board.new
+    board.board = ['X', 'X', 'X', 3, 'O', 'X', 'O', 'X', 'X']
+    assert_equal true, board.game_over?
+  end
+
+  # def test_is_game_over_x_wins
+  #   board = Board.new
+  #   board.board = ['X', 'X', 'X', 3, 'O', 'X', 'O', 'X', 'X']
+  #   assert_equal true, board.is_game_over?
+  # end
 end
 
