351e8c67047a8636c4463811c360d717d5e89041
maniginam <maniginam@gmail.com>
2021-05-19 22:45:08 -0500

    play turn should be working

--- test/game_master_test.rb
+++ test/game_master_test.rb
@@ -47,12 +47,23 @@ class GameMasterTest < Minitest::Test
     assert_equal 0, game_master.winner
   end
 
+  def test_o_turn
+    board = Board.new(['X', 'O', 'X', 3, 'O', 5, 'O', 'X', 'X'])
+    player1 = MockPlayer.new(:mock, 'X', 10)
+    player2 = MockPlayer.new(:mock, 'O', 10)
+    game_master = GameMaster.new(board, [player1, player2])
+    game_master.player = player2
+    assert_nil game_master.winner
+    game_master.run_game
+    assert_equal 0, game_master.winner
+  end
+
   # def test_x_turn
   #   board = Board.new(['X', 'O', 2, 3, 'O', 5, 'O', 'X', 'X'])
-  # player1 = MockPlayer.new(:mock, 'X', 10)
-  # player2 = MockPlayer.new(:mock, 'O', 10)
-  # game_master = GameMaster.new(board, [player1, player2])
-  #  game_master.run_game
+  #   player1 = MockPlayer.new(:mock, 'X', 10)
+  #   player2 = MockPlayer.new(:mock, 'O', 10)
+  #   game_master = GameMaster.new(board, [player1, player2])
+  #   game_master.run_game
   #   assert_nil game_master.winner
   #   assert_equal(-10, game_master.player)
   # end
--- test/spec_helper.rb
+++ test/spec_helper.rb
@@ -1,9 +1,15 @@
 # frozen_string_literal: true
 
+require_relative '../lib/minimax'
 class MockPlayer < Player
   attr_accessor :kind, :token, :score, :box
 
   def play_turn(board)
+    if @box.nil?
+      minimax = Minimax.new(board.board, @score)
+      scores = minimax.score_boxes
+      @box = minimax.choose_best_box(scores)
+    end
     board.board[@box] = @token
   end
 end
